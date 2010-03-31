5a409e1ac1c5e27eac877424474882488f317810
Nick Anderson <nick@cmdln.org>
2010-03-31 11:12:03 -0500

    added license

--- ilom_cmd.py
+++ ilom_cmd.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+# Copyright (c) 2010, Nick Anderson <nick@cmdln.org>
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import pexpect
 import sys
@@ -27,9 +49,9 @@ def process_args():
 def get_commands(commands_file):
     commands = []
     if commands_file:
-        with open(commands_file) as f:
-            commands = f.readlines()
-        f.closed
+        f = open(commands, "r"):
+        commands = f.readlines()
+        f.close()
     else:
         while True:
             line = sys.stdin.readline()
@@ -50,22 +72,22 @@ def exec_on_hosts(commands, hosts, options):
                 print 'ssh %s@%s' %(options.username,host)
             child = pexpect.spawn('ssh %s@%s' %(options.username,host))
             child.expect('(?i)Password:')
+            if options.verbose:
+                child.logfile = sys.stdout
             child.sendline(options.password)
 
             for command in commands:
-                child.expect('(?i)-> ')
-                if options.verbose:
-                    print command
+                child.expect(['-> ', 
+                              '\(y/n\)\?',])
                 child.sendline(command)
 
             child.expect('(?i)-> ')
             child.sendline('exit')
+            child.close()
         except:
             error='Error: at least one command failed to execute on %s\n' %host
             sys.stderr.write(error)
-            if options.verbose:
-                print "Debug Info"
-                print str(child)
+            child.close()
 
 
 def main():
